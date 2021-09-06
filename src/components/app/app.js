456d76392e4c441b036e0f38101ccea1382eb3e0
drakkar0 <drakkar0studio@gmail.com>
2021-09-06 12:10:22 +0300

    0609

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,26 @@
 import React, {Component} from "react";
-import SwapiServise from '../api';
+// import SwapiServise from '../../api';
+import Header from "../header";
+import ItemList from "../item-list";
+import RandomPlanet from "../random-planet";
+import PersonDetails from "../person-details";
 
+import './app.css'
 
 export default class App extends Component{
     render() {
-
-
-
-
         return(
-            <div>Hello
-        <SwapiServise />
+            <div className="stardb-app">
+                <Header />
+               <RandomPlanet />
+                <div className="row mb2">
+                    <div className="col-md-6">
+                        <ItemList />
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails />
+                    </div>
+                </div>
             </div>
 
         )
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/app';
-import SwapiServise from "./components/api";
+import SwapiServise from "./api";
+
+
 
 const swapi = new SwapiServise();
 swapi.getPerson(3).then((people)=>{
